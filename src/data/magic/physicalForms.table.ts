e97409765e7c3de9e5222488477bbf24c2323306
Thunderducky <scottzillaster@gmail.com>
2022-10-16 08:16:08 +0000

    added forms

---
+++ src/data/magic/physicalForms.table.ts
@@ -0,0 +1,49 @@
+import { listIt } from "../../utils"
+import { SimpleTable } from "../types"
+
+export const physicalFormTable: SimpleTable = {
+  name: "PhysicalForm",
+  entries: listIt`
+    Altar
+    Armor
+    Arrow
+    Beast
+    Blade
+    Cauldron
+
+    Chain
+    Chariot
+    Claw
+    Cloak
+    Colossus
+    Crown
+
+    Elemental
+    Eye
+    Fountain
+    Gate
+    Golem
+    Hammer
+
+    Horn
+    Key
+    Mask
+    Monolith
+    Pit
+    Prison
+
+    Sentinel
+    Servant
+    Shield
+    Spear
+    Steed
+    Swarm
+
+    Tentacle
+    Throne
+    Torch
+    Trap
+    Wall
+    Web
+  `
+}
